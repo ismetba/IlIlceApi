cd6f1adb2a690f5b2a22458012d4fe7897891e93
ismetba <ismet3170766@gmail.com>
2024-05-22 04:51:03 +0300

    added something for jenkins test

--- Presentation/WebApi.IlIlceApi/Controllers/WeatherForecastController.cs
+++ Presentation/WebApi.IlIlceApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "ismet","yusuf","mehmet","alper"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
